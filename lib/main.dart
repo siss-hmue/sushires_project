6383df3b6a5941114554584bb396df8c21a18b46
Siss-Hmue <6531503184@lamduan.mfu.ac.th>
2024-04-18 15:45:40 +0700

    User auth login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'components/appbackground.dart';
 import 'screens/firstloginpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
